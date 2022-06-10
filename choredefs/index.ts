2933b96db4937604904ccb402e9c8e29149b08d6
Tim Cuthbertson <tim@gfxmonk.net>
2022-06-10 11:30:18 +1000

    bump

--- choredefs/index.ts
+++ choredefs/index.ts
@@ -1,3 +1,3 @@
-import { defaultDockerOptions, default as Scala } from 'https://raw.githubusercontent.com/timbertson/chored-timbertson/09153fae3d365ce66bb9e4f4505e50da9e83a290/lib/scala.ts#main'
+import { defaultDockerOptions, default as Scala } from 'https://raw.githubusercontent.com/timbertson/chored-timbertson/f2709e673ff8d5dde43af021b4e7548afaf09ada/lib/scala.ts#main'
 
 export default Scala({ repo: 'auditspec' })
